aa1b8d1e1478081f2280bbf1e583655fb5854268
Sohyun Eom <sohy19@hufs.ac.kr>
2022-02-08 03:19:36 +0900

    solve

--- 7576 토마토.py
+++ 7576 토마토.py
@@ -1,56 +1,37 @@
-from copy import deepcopy
-import sys, copy
+from collections import deque
+import sys
 input = sys.stdin.readline
 
-def tomato(box, m, n):
-    new_box = copy.deepcopy(box)
-    cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if '1' not in box[i]:
-                break
-            if box[i][j] == '1':
-                if j < m-1:   # 오른쪽 이동
-                    if new_box[i][j+1] != '1' and box[i][j+1] != '-1':
-                        new_box[i][j+1] = '1'
-                        cnt += 1
-                if j > 0:   # 왼쪽 이동
-                    if new_box[i][j-1] != '1'and box[i][j-1] != '-1':
-                        new_box[i][j-1] = '1'
-                        cnt += 1
-                if i < n-1:   # 아래쪽 이동
-                    if new_box[i+1][j] != '1'and box[i+1][j] != '-1':
-                        new_box[i+1][j] = '1'
-                        cnt += 1
-                if i > 0:   # 위쪽 이동
-                    if new_box[i-1][j] != '1'and box[i-1][j] != '-1':
-                        new_box[i-1][j] = '1'
-                        cnt += 1
-    if cnt == 0:
-        return False
-    else:
-        return new_box
+def tomato():
+    if not q:
+        return
+    while q:
+        i, j = q.popleft()
+        for ni, nj in (i, j+1), (i+1, j), (i, j-1), (i-1, j):
+            if 0 <= ni < n and 0 <= nj < m:
+                if box[ni][nj] == '0':
+                    box[ni][nj] = int(box[i][j]) + 1
+                    q.append((ni, nj))
+    return int(box[i][j]) - 1
 
-
+# 입력
 m, n = map(int, input().split())   # 상자 가로 칸 수, 세로 칸 수
 box = []
-for _ in range(n):
+q = deque()
+for i in range(n):
     box.append(input().split())
+    if '1' in box[i]:
+        for j in range(m):
+            if box[i][j] == '1':
+                q.append((i, j))   # 처음 토마토가 익어있는 곳 (1인 곳) 저장
 
-day = 0   # 토마토가 모두 익을 때까지의 최소 날짜
-while True:
-    new_box = tomato(box, m, n)
-    if not new_box:
-        break
-    box = new_box
-    day += 1
-
-not_all = False   # 토마토 전체가 다 익었는지 확인
+# 출력
+result = tomato()
+not_all = False
 for i in range(n):
     if '0' in box[i]:
-        not_all = True
-
+        not_all = True   # 전체가 익지 못 함
 if not_all:
-    print('-1')
+    print(-1)
 else:
-    print(day)
+    print(result)
