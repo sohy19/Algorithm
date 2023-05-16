85f1c559c6144b5ec9ed925ac81b6c2419176be1
Sohyun Eom <88651937+sohy19@users.noreply.github.com>
2023-05-16 11:31:12 +0900

    Create 아름다운 수.py

---
+++ 코드트리/아름다운 수.py
@@ -0,0 +1,34 @@
+def is_beautiful():
+    for i in range(n):
+        if i == 0:
+            now_num = ans[i]
+            cnt = 1
+        elif now_num == ans[i]:
+            cnt += 1
+        else:
+            if cnt % now_num == 0:
+                cnt = 1
+                now_num = ans[i]
+                continue
+            else:
+                return False
+    if cnt % now_num == 0:
+        return True
+
+def choose_num(now):
+    global cnt
+    if now == n+1:
+        if is_beautiful():
+            cnt += 1
+        return
+    for i in range(1, 5):
+        ans.append(i)
+        choose_num(now+1)
+        ans.pop()
+
+
+n = int(input())
+ans = []
+cnt = 0
+choose_num(1)
+print(cnt)
