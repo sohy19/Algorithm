297519497163c779915f9a3d76049e56f3f2c130
Sohyun Eom <88651937+sohy19@users.noreply.github.com>
2024-04-24 22:40:33 +0900

    [solved] 23971 ZOAC 4.py

--- 백준/구현/23971 ZOAC 4.py
+++ 백준/구현/23971 ZOAC 4.py
@@ -5,3 +5,14 @@ h, w, n, m = map(int, input().split())   # h: 행, w: 열, n: 세로 거리두�
 rows = ceil(h / (n + 1))
 cols = ceil(w / (m + 1))
 print(rows * cols)
+
+# ------------------------------------------------------------
+
+import math
+
+h, w, n, m = map(int, input().split())
+
+width = math.ceil(w / (m + 1))
+height = math.ceil(h / (n + 1))
+
+print(width * height)
