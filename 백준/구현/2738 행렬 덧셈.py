aec6c792f277fe2f422d78f08ce92292b840f9a3
Sohyun Eom <88651937+sohy19@users.noreply.github.com>
2024-07-18 21:17:04 +0900

    [solved] 2738 행렬 덧셈.py

---
+++ 백준/구현/2738 행렬 덧셈.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(n)]
+B = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        print(A[i][j] + B[i][j], end=' ')
+    print()
