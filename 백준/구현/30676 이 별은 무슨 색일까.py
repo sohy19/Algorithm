11d55425af6a0ad3c1b77cef0956680ecb276774
Sohyun Eom <88651937+sohy19@users.noreply.github.com>
2024-09-21 23:48:03 +0900

    [solved] 30676 이 별은 무슨 색일까.py

---
+++ 백준/구현/30676 이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n >= 620 and n <= 780:
+    print("Red")
+elif n >= 590:
+    print("Orange")
+elif n >= 570:
+    print("Yellow")
+elif n >= 495:
+    print("Green")
+elif n >= 450:
+    print("Blue")
+elif n >= 425:
+    print("Indigo")
+else:
+    print("Violet")
